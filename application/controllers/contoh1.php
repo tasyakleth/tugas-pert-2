43f81873f18d794df473a08798158fe113239ec9
tasyakleth <tasyakyranie06@gmail.com>
2022-09-29 07:49:51 +0700

    contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "Nama saya Tasya Kyranie NA"
+		echo"Saya tinggal di daerah Sriwidari
+		olahraga yang saya sukai adalah
+		Jogging";
+	}
+}
